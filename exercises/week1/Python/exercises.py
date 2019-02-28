197b3f64bd947fcf64e1bf6a34465a55242693e3
DannyDannyDanny <s144222@student.dtu.dk>
2019-02-28 10:18:44 +0100

    update: exercise 2 done

--- exercises/week1/Python/exercises.py
+++ exercises/week1/Python/exercises.py
@@ -1,3 +1,5 @@
+from IPython.display import Markdown
+
 import numpy as np
 from scipy.spatial import distance
 import matplotlib.pyplot as plt
@@ -7,6 +9,13 @@ import os
 # If using as script and not from console;
 os.listdir('exercises/week1/')
 path = 'exercises/week1/python/'
+
+# %% Ex.1
+# a)
+# b)
+# c)
+
+# %%
 
 if __name__ == "__main__":
     diabetPath = path + 'DiabetesDataNormalized.txt'
@@ -43,3 +52,93 @@ if __name__ == "__main__":
     plt.show()
 
     print('MSE:',MSE)
+
+# %% Ex.2
+import scipy.linalg as lng
+
+if __name__ == "__main__":
+    n = 10 # nr of obs
+    p = 3 # features / vars
+    beta_true = np.array([1, 2, 3])
+    X = np.random.randn(n, p) # random drawing, feature matrix
+
+    m = 100 # nr of experiments
+
+    betas = np.zeros((p, m)) # all variable estimates
+
+    sigma = 0.1
+
+    for i in range(m):
+        # Measures response - true value plus noise level
+        y = np.matmul(X, beta_true) + sigma * np.random.randn(n)
+        betas[:, i] = lng.lstsq(X, y)[0] # Estimates
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 2, 1)
+    ax.set_title("All estimated betas")
+    lines = ax.scatter([range(m)] * 3, betas.T, marker = ".")
+
+    ax2 = fig.add_subplot(1,2,2)
+    ax2.set_title("Estimated betas as boxplot")
+    ax2.boxplot(betas.T)
+
+# %% Ex.3
+import scipy.linalg as lng
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    def ridgeMulti(X, _lambda, p, y):
+        inner_prod = np.linalg.inv(np.matmul(X.T, X) + _lambda * np.eye(p,p))
+        outer_prod = np.matmul(X.T, y)
+        betas = np.matmul(inner_prod, outer_prod)
+        return betas
+
+    diabetPath = path + 'DiabetesDataNormalized.txt'
+    T = np.loadtxt(diabetPath, delimiter = ' ', skiprows = 1)
+    y = T[:, 10]
+    X = T[:,:10]
+
+    [n, p] = np.shape(X)
+
+    off = np.ones(n)
+    M = np.c_[off, X] # Include offset / intercept
+
+    # Linear solver
+    beta_ols, res, rnk, s = lng.lstsq(M, y)
+
+    k = 100; # try k values of lambda
+    lambdas = np.logspace(-4, 3, k)
+
+    betas = np.zeros((p,k))
+
+    for i in range(k):
+        betas[:, i] = ridgeMulti(X, lambdas[i], p, y)
+
+    plt.figure()
+    plt.semilogx(lambdas, betas.T )
+    plt.xlabel("Lambdas")
+    plt.ylabel("Betas")
+
+    # Bias and variance of the ridge regression, same as exercise 2 - just for ridge
+    n = 10 # nr of obs
+    p = 3 # features / vars
+    beta_true = np.array([1, 2, 3])
+    X = np.random.randn(n, p) # random drawing, feature matrix
+
+    m = 100 # nr of experiments
+
+    betas2 = np.zeros((k, p, m)) # all variable estimates
+
+    sigma = 0.2
+
+    for i in range(k):
+        for j in range(m):
+            y = np.matmul(X, beta_true) + sigma * np.random.randn(n) # Measures response - true value plus noise level
+            betas2[i, :, j] = ridgeMulti(X, lambdas[i], p, y)
+
+    betas_mean = np.mean(betas2, axis = 2)
+
+    plt.figure()
+    plt.semilogx(lambdas, betas_mean)
+    plt.xlabel("Lambdas")
+    plt.ylabel("Betas")
