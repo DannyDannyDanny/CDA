f0bd844f138fb631938ce7635abc62adf43d649a
DannyDannyDanny <s144222@student.dtu.dk>
2019-04-04 11:16:32 +0200

    update: case1 closed

--- case1_master/case1_2018.py
+++ case1_master/case1_2018.py
@@ -1,13 +1,11 @@
-# %%
+# %% IMPORTS AND FUNCTIONS
 import numpy as np
 import pandas as pd
 from sklearn import linear_model
 from sklearn.model_selection import KFold
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
-# %% markdown
-# # Function definitions
-# %%
+
 def RMSE(y_true, y_pred):
     """
         Relative mean squared error (RMSE)
@@ -16,7 +14,7 @@ def RMSE(y_true, y_pred):
     denominator = np.sqrt(np.mean(np.power(y_true-np.mean(y_true), 2)))
     rmse = numerator / denominator
     return rmse
-# %%
+
 def CV(model, X, y, K, plot, **kwargs):
     """
         Cross-validation
@@ -63,27 +61,19 @@ def CV(model, X, y, K, plot, **kwargs):
         plt.show()
 
     return np.mean(train_error), np.mean(test_error)
-# %%
+
 kft = KFold(100, shuffle=True)
 
-i = 0
-for train, test in kft.split(X):
-    print(i, train, test)
-    i += 1
 # %% LOADING DATA
 data = pd.read_csv("case1_master/case1/dataCase1.csv")
-# data18 = pd.read_csv('case1_master/2018/Case1_Data.csv')
-data.shape
-data18.shape
 
-# %% TRANSFORMING CATEGORICAL COLUMNS
-# data['X96'].unique()
-# data['X97'].unique()
-# data['X98'].unique()
-# data['X99'].unique()
-# data['X100'].unique()
+delme = set()
+for col_name in cat_cols:
+    delme = delme.union(set(data[col_name].unique()))
 
-# FILL CATEGORICAL VALUES WITH MOST POPULAR VALUE
+
+
+# %% FILL CATEGORICAL NANs VALUES WITH MOST POPULAR VALUE
 cat_cols = ['X96','X97','X98','X99','X100']
 for col_name in cat_cols:
     filler = data[col_name].value_counts().idxmax()
@@ -97,20 +87,9 @@ for col_name in cat_cols:
         data[col_name + unique_val] = (data[col_name].values==unique_val)*1
     del data[col_name]
 
-# %% 1.2 Cleaning data
-data.describe()
-# %% Some of the columns are missing values, hence not all columns have 100 observations.
-
-
-# #### Checking for NaN values
-# %%
-## FIND NAN VALUES
-data[data.iloc[:,1:].isnull().any(axis=1)]
-# %% markdown
-# #### Replacing NaN values with mean value of corresponding column.
-# %%
+# %% REPLACE NUMERICAL NAN-VALUES WITH MEAN VALUES
 data_clean = data
-data_clean.iloc[:,1:] = data_clean.iloc[:,1:].fillna(data_clean.iloc[:,1:].mean())#.values;
+data_clean.iloc[:,1:] = data_clean.iloc[:,1:].fillna(data_clean.iloc[:,1:].mean())
 
 X = data_clean[data_clean["Y"].notnull()].iloc[:,1:].values
 y = data_clean[data_clean["Y"].notnull()]["Y"]
@@ -119,36 +98,24 @@ y = y.values
 Xn = data_clean[data_clean["Y"].isnull()].iloc[:,1:].values
 yn = data_clean["Y"][data_clean["Y"].isnull()].values
 
-print("X shape:", X.shape)
-print("y shape:", y.shape)
-print("Xn shape:", Xn.shape)
-print("yn shape:", yn.shape)
-# %% markdown
-# ## 1.3 Standardization of X
-# %%
+# %% X STANDARDIZATION
 X = (X - X.mean(axis=0)) / X.std(axis=0)
 Xn = (Xn - Xn.mean(axis=0)) / Xn.std(axis=0)
-# %% markdown
-# # 2 Data Modelling
-# %%
+
+# %% MODELLING
 model_scores = []
 K = 10
-# %% markdown
-# ## 2.1 OLS
-# %%
-## OLS ##
-reg_ols = linear_model.LinearRegression
 
+# %% OLS
+reg_ols = linear_model.LinearRegression
 train_error, test_error = CV(reg_ols, X, y, K, fit_intercept=True, plot=False)
 model_scores.append({"model":"OLS", "lambda":0, "train_error":train_error, "test_error":test_error})
-print("train RMSE={:.3f}\t test RMSE={:.3f}".format(train_error, test_error))
-# %% markdown
-# ## 2.2 LARS
-# %%
-## LARS ##
+print("train RMSE={:.3f}\ntest RMSE={:.3f}".format(train_error, test_error))
+
+# %% LARS
 reg_lars = linear_model.Lars
 
-lambdas = range(1,40,1)
+lambdas = range(1,50,1)
 train_errors, test_errors = [], []
 for lambda_ in lambdas:
     train_error, test_error = CV(reg_lars, X, y, K, n_nonzero_coefs=lambda_, fit_intercept=True, plot=False)
@@ -157,20 +124,19 @@ for lambda_ in lambdas:
     model_scores.append({"model":"LARS", "lambda":lambda_, "train_error":train_error, "test_error":test_error})
 
 plt.figure(figsize=(9,5))
-plt.plot(lambdas, train_errors)
-plt.plot(lambdas, test_errors)
+plt.plot(lambdas, train_errors,label='train errors')
+plt.plot(lambdas, test_errors,label='test errors')
+plt.legend()
 plt.title("LARS Cross-validation")
 plt.xlabel("Lambda (non-zero coefficients)")
 plt.ylabel("RMSE")
 plt.savefig("lars_cv.png")
 plt.show()
-# %% markdown
-# ## 2.3 RIDGE
-# %%
-## RIDGE ##
+
+# %% RIDGE
 reg_ridge = linear_model.Ridge
 
-lambdas = np.arange(0.1,4,0.05)
+lambdas = np.arange(0.1,30,0.05)
 train_errors, test_errors = [], []
 for lambda_ in lambdas:
     train_error, test_error = CV(reg_ridge, X, y, K, alpha=lambda_, fit_intercept=True, plot=False)
@@ -179,16 +145,16 @@ for lambda_ in lambdas:
     model_scores.append({"model":"RIDGE", "lambda":lambda_, "train_error":train_error, "test_error":test_error})
 
 plt.figure(figsize=(9,5))
-plt.plot(lambdas, train_errors)
-plt.plot(lambdas, test_errors)
+plt.plot(lambdas, train_errors, label = 'train error')
+plt.plot(lambdas, test_errors, label = 'test error')
 plt.title("Ridge Regression Cross-validation")
 plt.xlabel("Lambda (L2-norm)")
 plt.ylabel("RMSE")
+plt.legend()
 plt.savefig("ridge_cv.png")
 plt.show()
-# %% markdown
-# ## 2.4 ELASTIC NET
-# %%
+
+# %% ELASTIC NET
 manual_elastic = linear_model.ElasticNet
 CV(manual_elastic, X, y, 10, alpha=0.1, l1_ratio=0.9, fit_intercept=True, max_iter=700, plot=True)
 # %%
@@ -196,7 +162,7 @@ CV(manual_elastic, X, y, 10, alpha=0.1, l1_ratio=0.9, fit_intercept=True, max_it
 reg_elastic = linear_model.ElasticNet
 
 lambdas = np.arange(0.1, 1.5, 0.05)
-kappas = np.arange(0.1, 1, 0.05)
+kappas = np.arange(0.6, 1.5, 0.05)
 for lambda_ in lambdas:
     for kappa in kappas:
         train_error, test_error = CV(reg_elastic, X, y, K, alpha=lambda_, l1_ratio=kappa, max_iter=1000, fit_intercept=True, plot=False)
@@ -209,32 +175,35 @@ kappas = list(map(lambda x: x["kappa"], elastics))
 tste = list(map(lambda x: x["test_error"], elastics))
 
 plt.figure(figsize=(9,5))
-plt.scatter(lambdas, kappas, c=tste)
+plt.scatter(lambdas, kappas,s=80, c=tste,cmap='RdBu')
 plt.title("ElasticNet Cross-validation")
 plt.xlabel("Lambda 1 (L2-norm)")
 plt.ylabel("Lambda 2 (L1-norm)")
-plt.gray()
-cbar= plt.colorbar()
+colorbar = 'RdBu'
+cbar = plt.colorbar()
 cbar.ax.set_ylabel("RMSE")
 plt.savefig("elastic_cv.png")
 plt.show()
-# %% markdown
-# ## 2.5 Best model
-# %% markdown
-# ### top 5
-# %%
+# %% TOP 5 MODELS
 top5 = sorted(model_scores, key=lambda x: x["test_error"])[:5]
 print("Top 5 models:")
 top5
-# %% markdown
-# ### top 1
+
 # %%
+for i in top5:
+    i0 = 'ElasticNet'
+    i1 = round(i['lambda'],4)
+    i2 = round(i['kappa'],4)
+    i3 = round(i['train_error'],4)
+    i4 = round(i['test_error'],4)
+    print(i0,'&',i1,'&',i2,'&',i3,'&',i4,'\\\\')
+
+# %% TOP MODEL
 top1 = sorted(model_scores, key=lambda x: x["test_error"])[0]
 print("Best model:")
 top1
-# %% markdown
-# ### Choose with $+1\sigma$
-# %%
+
+# %% ONE STANDARD ERROR RULE
 cv_std = np.std(list(map(lambda x: x["test_error"], elastics)))
 print("Test error standard deviation:", cv_std)
 
@@ -243,11 +212,53 @@ print("Choose least complex model closest to a performance of:", onestd)
 
 top1std = list(filter(lambda x: x["test_error"] > onestd, elastics))[0]
 top1std
+
+# %%
+
+for i in [top1std]:
+    i0 = 'ElasticNet'
+    i1 = round(i['lambda'],4)
+    i2 = round(i['kappa'],4)
+    i3 = round(i['train_error'],4)
+    i4 = round(i['test_error'],4)
+    print(i0,'&',i1,'&',i2,'&',i3,'&',i4,'\\\\')
+
 # %%
 model1std = linear_model.ElasticNet(alpha=top1std["lambda"], l1_ratio=top1std["kappa"])
 model1std.fit(X, y)
 # %%
+for i in model1std.sparse_coef_:
+    print(i)
+# %%
+s = """
+  (0, 2)	1.4121946510707377
+  (0, 42)	0.6602090684585923
+  (0, 56)	3.039021800143413
+  (0, 100)	-0.3415358118525393
+  (0, 103)	0.28130981785499043
+  (0, 107)	0.038981939776535496
+  (0, 112)	0.09307844239561533
+"""
+
+# data.columns
+
+for i in s.split('\n')[1:-1]:
+    i = str(i.replace('\t','').replace(' ',''))
+    i = i.split('(')[1].split(')')
+    i[0] = int(i[0].split(',')[1])
+    i[0] = data.columns[i[0]].replace('X','$X_{')+'}$'
+
+    i[1] = round(float(i[1]),3)
+    print(i[0],'&',i[1],'\\\\')
+    # i[0] = '$x_{'+i[0]+'}$'
+    # print(i[0],'\t',i[1])
+# %%
 predn = model1std.predict(Xn)
+# %%
+path = 'case1_master/case1/results/'
+pred_path = path + 'prediction.csv'
+predn.tofile(pred_path,'\n')
+
 # %%
 fig = plt.figure(figsize=(13,8))
 plt.plot(predn)
