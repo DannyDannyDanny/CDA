62dbea6635c6ecbcfb581a50c9805145a43a1169
DannyDannyDanny <s144222@student.dtu.dk>
2019-05-03 21:21:31 +0200

    wip: loading subset of august data

--- case_2_new/case2.py
+++ case_2_new/case2.py
@@ -13,3 +13,28 @@ for file in file_list:
     print('----> reading: ',file)
     files_dict[file] = pd.read_csv(file,nrows=20,encoding = "ISO-8859-1")
     print(files_dict[file].dtypes)
+
+
+import pandas
+import random
+
+# loads header + s random lines from each file
+s = 100 #desired sample size
+for filename in file_list:
+    print('----> counting lines in:',file)
+    #number of records in file (excludes header)
+    n = sum(1 for line in open(filename)) - 1
+    print('---->',n,'lines counted, sampling',n,'lines')
+    skip = sorted(random.sample(range(1,n+1),n-s))
+    #the 0-indexed header will not be included in the skip list
+    df = pandas.read_csv(filename, skiprows=skip, encoding = "ISO-8859-1")
+    print(files_dict[file].dtypes)
+
+for k,v in files_dict:
+    print(k,v.shape)
+
+
+# vector of visitors of 1/0s for customer/non customer
+Y = []
+
+# matrix of properties belonging to person
