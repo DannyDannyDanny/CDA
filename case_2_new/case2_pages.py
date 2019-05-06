de2880b5fd4c1d6286e14316f537bf637f3fc385
DannyDannyDanny <s144222@student.dtu.dk>
2019-05-06 14:43:53 +0200

    feat: accuracy metric + cleaning

--- case_2_new/case2_pages.py
+++ case_2_new/case2_pages.py
@@ -7,23 +7,14 @@ path = './case_2_new/data/'
 os.chdir(path)
 # %%
 file_list = [c for c in os.listdir('.') if c[-3:]=='csv']
-# [c[:-7]+'aug.csv' for c in os.listdir('.') if c[-3:]=='csv']
-# %%
-s = 100000 #desired sample size
+s = 10000 #desired sample size
 e = "ISO-8859-1"
 df_1 = pd.read_csv("page_aug.csv",
                  nrows=s,
                  encoding =e,
                  error_bad_lines = False)
 
-# df_1.referringpageinstanceid.nunique()
-# df_1.pagetitle.nunique()
-# df_1.referringpageinstanceid.fillna(0,inplace=True)
-# df_1.drop(columns=['sessionnumber','eventtimestamp'])
-# df_1.describe()
-# df_1.pagetitle.nunique()
-# df_1.pagetitle
-# df_1.pagelocationdomain.unique()
+df_1.referringpageinstanceid.fillna(0,inplace=True)
 X = pd.get_dummies(df_1).drop(columns=['sessionnumber','eventtimestamp','iscustomer'])
 y = np.array(pd.get_dummies(df_1)['iscustomer'])
 X.shape
@@ -50,18 +41,28 @@ print('Training Labels Shape:', train_labels.shape)
 print('Testing Features Shape:', test_features.shape)
 print('Testing Labels Shape:', test_labels.shape)
 
+# %%
+df_1.iscustomer.value_counts()
+# %%
 # Import the model we are using
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.feature_selection import SelectFromModel
+from sklearn.metrics import confusion_matrix
 # Instantiate model with 1000 decision trees
 rf = RandomForestRegressor(n_estimators=1000, random_state=42)
-
-# Train the model on training data
 rf.fit(train_features, train_labels)
 pred_labels = rf.predict(test_features)
-pred_labels = [int(l) for l in pred_labels]
-test_labels = [int(l) for l in test_labels]
+pred_labels.round()
 
-from sklearn.metrics import confusion_matrix
-tn, fp, fn, tp = confusion_matrix(test_labels,pred_labels).ravel()
+
+tn, fp, fn, tp = confusion_matrix(test_labels,pred_labels.round()).ravel()
+# %%
 print('accuracy:',(tp+tn)/sum([tn, fp, fn, tp]))
-confusion_matrix(test_labels,pred_labels)
+confusion_matrix(test_labels,pred_labels.round())
+
+sel = SelectFromModel(RandomForestRegressor(n_estimators=1000, random_state=42))
+sel.fit(train_features, train_labels)
+sel.get_support()
+
+
+# %%
